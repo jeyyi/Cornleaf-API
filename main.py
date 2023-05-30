2250029af34bf650c2603ec4b9b345e202a3ac34
Jayrhald Padilla <jayrhald.jp@gmail.com>
2023-05-30 13:32:27 +0800

    Added classify but with error for Mac

--- main.py
+++ main.py
@@ -2,10 +2,30 @@ from typing import Union
 import tensorflow as tf
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
+from tensorflow.keras.preprocessing.image import img_to_array
 from fastapi import FastAPI, UploadFile, File
+from fastapi.middleware.cors import CORSMiddleware
+import os
+from PIL import Image
+import io
+import numpy as np
 
 app = FastAPI()
 
+
+# Configure CORS
+origins = [
+    "http://localhost:3000",  # Replace with the URL of your React app
+    # Add more allowed origins if needed
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 #load the pretrained model
 model = load_model('Models/Inception.h5')
 labels = ['Gray Leaf Spot', 'Common Rust', 'Healthy', 'Blight']
@@ -16,17 +36,22 @@ def read_root():
 
 @app.post("/classify")
 async def classify_image(file: UploadFile = File(...)):
-    #preprocess the image
-    img = image.load_img(file.file, target_size=(180,180))
-    x = image.img_to_array(img)
-    x = x/255.0
-    x = tf.expand_dims(x, axis= 0)
+    with tf.device("/cpu:0"):
+        # Read the image file
+        image = await file.read()
+        img = Image.open(io.BytesIO(image))
 
-    #perform the classification
-    prediction = model.predict(x)
-    predicted_class_index = tf.argmax(prediction, axis=1)[0]
-    predicted_class_label = labels[predicted_class_label]
+        # Resize the image to the input size expected by the Inception model
+        img = img.resize((180, 180))
 
-    return {"predicted_class": predicted_class_label}
+        # Preprocess the image
+        x = img_to_array(img)
+        x = x / 255.0  # Normalize the pixel values between 0 and 1
+        x = np.expand_dims(x, axis=0)
 
+        # Make predictions using the Inception model
+        prediction = model.predict(x)
+        predicted_class_index = np.argmax(prediction, axis=1)[0]
+        predicted_class_label = labels[predicted_class_index]
 
+        return {"predicted_class": predicted_class_label}
